b7e67e502daa454981a39755be45c569053e3fb0
Daniel <d.lalime7@gmail.com>
2020-07-09 10:04:41 -0700

    done

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEventsDemo.Controllers
@@ -16,11 +17,46 @@ namespace CodingEventsDemo.Controllers
         {
             context = dbContext;
         }
+
+
         //[HttpGet("EventCategory/Index")]
         public IActionResult Index()
         {
             List<EventCategory> Categories = context.Categories.ToList();
             return View(Categories);
         }
+        //GET: 
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel viewModel = new AddEventCategoryViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost("/EventCategory/Create")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel viewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                EventCategory eventCategory = new EventCategory
+                {
+                    Name = viewModel.Name
+
+                };
+                context.Categories.Add(eventCategory);
+                context.SaveChanges();
+                
+                return Redirect("/EventCategory");
+
+            }
+            return View("Create", viewModel);
+
+
+        }
     }
 }
+
+
+
+
--- CodingEventsDemo/Models/EventCategory.cs
+++ CodingEventsDemo/Models/EventCategory.cs
@@ -22,16 +22,6 @@ namespace CodingEventsDemo.Models
             Name = name;
         }
 
-        public override bool Equals(object obj)
-        {
-            return obj is EventCategory category &&
-                   Id == category.Id &&
-                   Name == category.Name;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id, Name);
-        }
+       
     }
 }
